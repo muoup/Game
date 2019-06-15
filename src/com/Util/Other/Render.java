e53d14769dea8af4805e262a01eae4e379e5eed1
masterdust12 <zbv1106@yahoo.com>
2019-06-14 22:42:43 -0400

    Quite a lofty commit (adding new files):
    Added NPCs nicely
    Made graphic rendering static via Render.(method)
    Commented some things, reworked everything so a graphics
    object is not needed.
    
    Fixed:
    There was a slight issue with the offset when reaching the
    end of the map, that is fixed.

---
+++ src/com/Util/Other/Render.java
@@ -0,0 +1,40 @@
+package com.Util.Other;
+
+import com.Game.Main.Main;
+import com.Util.Math.Vector2;
+
+import java.awt.*;
+
+public class Render {
+    public static void drawImage(Image image, Vector2 position) {
+        Main.graphics.drawImage(image, (int) position.x, (int) position.y, null);
+    }
+
+    public static void drawImage(Image image, float x, float y) {
+        Main.graphics.drawImage(image, (int) x, (int) y, null);
+    }
+
+    public static void drawRectangle(Vector2 position, Vector2 position2) {
+        Main.graphics.fillRect((int) position.x, (int) position.y, (int) position2.x, (int) position2.y);
+    }
+
+    public static void setColor(Color color) {
+        Main.graphics.setColor(color);
+    }
+
+    public static void setFont(Font font) {
+        Main.graphics.setFont(font);
+    }
+
+    public static void drawText(String text, Vector2 position) {
+        Main.graphics.drawString(text, (int) position.x, (int) position.y);
+    }
+
+    public static void drawText(String text, float x, float y) {
+        Main.graphics.drawString(text, (int) x, (int) y);
+    }
+
+    public static Vector2 getDimensions(Image image) {
+        return new Vector2(image.getWidth(null), image.getHeight(null));
+    }
+}
