6164f4636db87a34cf11f259b3818aef0760097b
masterdust12 <zbv1106@yahoo.com>
2019-09-22 21:51:18 -0700

    The game is actually in a playable state now. Some crashing has been fixed (thought not all). The game can be compiled with JDK 8 and the multiplayer has been tested and works.

---
+++ src/com/Game/Projectile/RockArrow.java
@@ -0,0 +1,14 @@
+package com.Game.Projectile;
+
+import com.Util.Math.Vector2;
+
+public class RockArrow extends Projectile {
+    public RockArrow(Vector2 position, Vector2 aim, float damage, float speed, float expMultiplier, boolean friendly) {
+        super(position, aim, damage, speed, expMultiplier, friendly);
+        this.rotate = true;
+        this.duration = 1.25f;
+        this.attackStyle = 1;
+        this.scale = Vector2.identity(4);
+        setImage("rock_arrow.png");
+    }
+}
