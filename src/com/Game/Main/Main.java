3394d54f94f81d8cf6e9acdfbd0463a8ee8c592c
Connor McDermid <cam-o-man@live.com>
2019-10-31 14:52:43 -0400

    Updated the server IP to the server!
    Signed-off-by: Connor McDermid <cam-o-man@live.com>

--- src/com/Game/Main/Main.java
+++ src/com/Game/Main/Main.java
@@ -28,7 +28,7 @@ public class Main extends Canvas {
     public static final String connectionCode = "69";
     public static final String messageCode = "13";
     public static boolean isConnected = false;
-    public static final String ipAddress = "localhost";
+    public static final String ipAddress = "hacksugar.asuscomm.com";
 
     public static Player player;
     public static Menu settings;
