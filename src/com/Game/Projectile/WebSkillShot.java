7c1e76c3291b61c8beb53b4f0daf18fa409bb6fd
masterdust12 <zbv1106@yahoo.com>
2020-01-01 20:30:53 -0500

    add

---
+++ src/com/Game/Projectile/WebSkillShot.java
@@ -0,0 +1,13 @@
+package com.Game.Projectile;
+
+import com.Util.Math.Vector2;
+
+public class WebSkillShot extends Projectile {
+    public WebSkillShot(Vector2 position, Vector2 aim) {
+        super(position, aim, 12, 2.25f, 0, false);
+        this.duration = 2.5f;
+        this.rotate = false;
+        setScale(18);
+        setImage("web.png");
+    }
+}
